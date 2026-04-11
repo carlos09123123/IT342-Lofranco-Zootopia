09e718394ab12e30ade6546c56a3be22a5b72281
Carlos <163004496+carlos09123123@users.noreply.github.com>
2026-04-11 21:17:06 +0800

    IT342 Phase 3 – Web Main Feature Completed

--- app/src/main/java/com/example/zootopiamobile/api/AuthResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.zootopiamobile.api;
-
-// This file was previously containing SignupRequest, LoginRequest and LoginResponse.
-// Those classes have been moved to their own files to comply with Java naming conventions.
